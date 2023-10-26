e78061df9c2efe50f815439c8dc013d8953610ae
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-26 12:04:59 +0530

    Modified

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:to_do_app/screens/home.dart';
 
 void main() {
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'ToDo App', home: Home());
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, title: 'ToDo App', home: Home());
   }
 }
