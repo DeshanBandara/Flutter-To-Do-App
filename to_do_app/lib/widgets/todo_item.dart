257db8ede52ba9bec41eeb5eb5c6270a5f9a4691
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2023-10-27 10:23:59 +0530

    Updated

--- to_do_app/lib/widgets/todo_item.dart
+++ to_do_app/lib/widgets/todo_item.dart
@@ -1,34 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/model/todo.dart';
 import '../constants/colors.dart';
 
 class ToDoItem extends StatelessWidget {
-  const ToDoItem({Key? key}) : super(key: key);
+  final ToDo todo;
+
+  const ToDoItem({Key? key, required this.todo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
-        onTap: () {},
+        onTap: () {
+          print('Clicked on ToDo Item.');
+        },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20,vertical:5),
+        //Check Box
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
           Icons.check_box,
           color: tdBlue,
         ),
         title: Text(
-          'Check Mail',
+          todo.todoText!,
           style: TextStyle(
             fontSize: 16,
             color: tdBlack,
             decoration: TextDecoration.lineThrough,
           ),
         ),
+        //Recycle bin
         trailing: Container(
           padding: EdgeInsets.all(0),
-          margin:EdgeInsets.symmetric(vertical: 6),
+          margin: EdgeInsets.symmetric(vertical: 6),
           height: 35,
           width: 35,
           decoration: BoxDecoration(
@@ -39,7 +47,9 @@ class ToDoItem extends StatelessWidget {
             color: Colors.white,
             iconSize: 18,
             icon: Icon(Icons.delete),
-            onPressed: () {},
+            onPressed: () {
+              print('Click on delete icon.');
+            },
           ),
         ),
       ),
